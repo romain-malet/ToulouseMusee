63ff86f333a3ceea20cc3bd70d1149a7f857047a
meradi <rabahmeradi@gmail.com>
2015-04-08 15:08:53 +0200

    Implemented Musee

--- grails-app/domain/toulousemusee/Musee.groovy
+++ grails-app/domain/toulousemusee/Musee.groovy
@@ -3,10 +3,20 @@ package toulousemusee
 class Musee {
 	
 	String nom
-	String 
+	String horairesOuverture
+	String telephone
+	String accesBus
+	String accesMetro
+	
+	Adresse adresse
 
     static hasMany = [ddeVisitMusee: DemandeVisiteMusee]
 
     static constraints = {
+		nom blank:false
+		horairesOuverture blank:false
+		telephone blank:false, matches:'0\\d\\s*\\d{2}\\s*\\d{2}\\s*\\d{2}\\s*\\d{2}'
+		accesBus blank:false
+		accesMetro blank:false
     }
 }
