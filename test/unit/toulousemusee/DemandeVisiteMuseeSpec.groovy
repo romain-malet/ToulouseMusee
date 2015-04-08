f3765c11f66232f245ebc7e3996cd25d8b355448
Romain <romain.malet@yahoo.fr>
2015-04-08 13:51:06 +0200

    Add domain class DemandeVisiteMusee

--- test/unit/toulousemusee/DemandeVisiteMuseeSpec.groovy
+++ test/unit/toulousemusee/DemandeVisiteMuseeSpec.groovy
@@ -6,10 +6,15 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Gestionnaire)
-class GestionnaireSpec extends Specification {
+@TestFor(DemandeVisiteMusee)
+class DemandeVisiteMuseeSpec extends Specification {
 
-    void "test l'invalidité d'un gestionnaire non valide"(String ) {
+    def setup() {
+    }
 
+    def cleanup() {
+    }
+
+    void "test something"() {
     }
 }
