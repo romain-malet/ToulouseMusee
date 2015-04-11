8266f40103f846379acef866305c7835b38359f3
Romain <romain.malet@yahoo.fr>
2015-04-11 16:38:51 +0200

    Edit BootStrap to initialize data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import toulousemusee.InitialiseDonneesService
+
 class BootStrap {
 
+    InitialiseDonneesService initialiseDonneesService
+
     def init = { servletContext ->
+        initialiseDonneesService.initData()
     }
     def destroy = {
     }
