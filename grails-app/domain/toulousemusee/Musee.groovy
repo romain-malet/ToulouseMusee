33193f786b87392ce659990ea557678d9a36e3f0
meradi <rabahmeradi@gmail.com>
2015-04-11 16:07:42 +0200

    Added hasOne to Musee

--- grails-app/domain/toulousemusee/Musee.groovy
+++ grails-app/domain/toulousemusee/Musee.groovy
@@ -1,23 +1,26 @@
 package toulousemusee
 
 class Musee {
-	
+
 	String nom
 	String horairesOuverture
 	String telephone
 	String accesBus
 	String accesMetro
-	
+
 	Adresse adresse
-    Gestionnaire gestionnaire
+	Gestionnaire gestionnaire
 
-    static hasMany = [ddeVisitMusee: DemandeVisiteMusee]
+	static hasOne = [gestionnaire:Gestionnaire]
 
-    static constraints = {
+	static hasMany = [ddeVisitMusee: DemandeVisiteMusee]
+
+	static constraints = {
 		nom blank:false
 		horairesOuverture blank:false
 		telephone blank:false, matches:'0\\d\\s*\\d{2}\\s*\\d{2}\\s*\\d{2}\\s*\\d{2}'
 		accesBus blank:false
 		accesMetro blank:false
-    }
+		gestionnaire nullable:false
+	}
 }
