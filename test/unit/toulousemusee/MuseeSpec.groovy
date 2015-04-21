fade02b98f612b9a0e8bef315d53520f565a6cb2
meradi <rabahmeradi@gmail.com>
2015-04-21 10:00:53 +0200

    Fixed unit tests for Musee

--- test/unit/toulousemusee/MuseeSpec.groovy
+++ test/unit/toulousemusee/MuseeSpec.groovy
@@ -13,10 +13,16 @@ class MuseeSpec extends Specification {
 
 	@Unroll
 	void "test musée valide"(String nom, String horairesOuverture, String telephone,
-                             String accesBus, String accesMetro) {
+			String accesBus, String accesMetro) {
 		given: "Un musée valide avec nom, horaires d'ouverture, téléphone, accès bus et metro"
+		Adresse uneAdresse = new Adresse(
+				numero: 42,
+				rue: "Boulevard du péril jeune",
+				codePostale: 31000,
+				ville: "Toulouse"
+				)
 		Musee musee = new Musee(nom: nom, horairesOuverture: horairesOuverture, telephone: telephone,
-                accesBus: accesBus, accesMetro: accesMetro, adresse:Mock(Adresse))
+		accesBus: accesBus, accesMetro: accesMetro, gestionnaire:Mock(Gestionnaire), adresse:uneAdresse)
 
 		expect: "le musée est valide"
 		musee.validate() == true
@@ -25,18 +31,18 @@ class MuseeSpec extends Specification {
 		nom   | horairesOuverture | telephone        | accesBus | accesMetro
 		"nom" | "9h 18h"          | "0514782314"     | "Bus 63" | "A"
 		"nom" | "9h 18h"          | "05 14 78 23 14" | "Bus 63" | "A"
-        "nom" | "9h 18h"          | "0514782314"     | null     | "A"
-        "nom" | "9h 18h"          | "05 14 78 23 14" | "Bus 63" | null
+		"nom" | "9h 18h"          | "0514782314"     | null     | "A"
+		"nom" | "9h 18h"          | "05 14 78 23 14" | "Bus 63" | null
 	}
 
 	@Unroll
 	void "test musée invalide"(String nom, String horairesOuverture, String telephone,
-							   String accesBus, String accesMetro, Adresse adresse,
-                               Gestionnaire gestionnaire){
+			String accesBus, String accesMetro, Adresse adresse,
+			Gestionnaire gestionnaire){
 		given: "Un musée est valide avec un nom, les horaires d'ouverture, un numéro de téléphone et les accès bus et metro"
 		Musee musee = new Musee(nom: nom, horairesOUvertures: horairesOuverture,
-			telephone: telephone, accesBus: accesBus, accesMetro: accesMetro,
-			adresse: adresse, gestionnaire: gestionnaire)
+		telephone: telephone, accesBus: accesBus, accesMetro: accesMetro,
+		adresse: adresse, gestionnaire: gestionnaire)
 
 		expect:"le musée est valide"
 		musee.validate() == false
@@ -52,6 +58,6 @@ class MuseeSpec extends Specification {
 		"nom"  | "9h 18h"          | "un numéro"      | "Bus 63" | "A"        | Mock(Adresse) | Mock(Gestionnaire)
 		"nom"  | "9h 18h"          | "05 47 82 3"     | "Bus 63" | "A"        | Mock(Adresse) | Mock(Gestionnaire)
 		"nom"  | "9h 18h"          | "05 47 82 32 15" | "Bus 63" | "A"        | null          | Mock(Gestionnaire)
-        "nom"  | "9h 18h"          | "05 47 82 32 15" | "Bus 63" | "A"        | Mock(Adresse) | null
+		"nom"  | "9h 18h"          | "05 47 82 32 15" | "Bus 63" | "A"        | Mock(Adresse) | null
 	}
 }
